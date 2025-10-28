7b4f22555b8d0fbd9bad3ea7e45fbf5b1aa7540e
shwetgaur <shwetgaur9@gmial.com>
2025-10-29 01:29:39 +0530

    new

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
 HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 YOUR_HF_MODEL_ID = "shwetgaur/SG_OG_name_generator"
 # NEW, CORRECTED URL
-HF_API_URL = f"https://router.huggingface.co/hf-inference/models/{YOUR_HF_MODEL_ID}"
+HF_API_URL = f"https://api-inference.huggingface.co/models/{YOUR_HF_MODEL_ID}"
 DOMAINR_API_URL = "https://domainr.p.rapidapi.com/v2/status"
 
 app = FastAPI(title="BizBrand.ai API")
